130cbaa35fa633b05271051084d7271992f49740
Andrew Stuart <andrew.stuart2@gmail.com>
2017-05-20 12:30:39 -0700

    Fix bufmap implementation

--- pkg/bufmap.go
+++ pkg/bufmap.go
@@ -3,12 +3,13 @@ package impl
 import (
 	"io/ioutil"
 	"log"
+	"path"
 )
 
 type Buffers map[string][]byte
 
-func (b Buffers) Overlay(path string) error {
-	fi, err := ioutil.ReadDir(path)
+func (b Buffers) Overlay(p string) error {
+	fi, err := ioutil.ReadDir(p)
 	if err != nil {
 		return err
 	}
@@ -16,7 +17,7 @@ func (b Buffers) Overlay(path string) error {
 	for _, info := range fi {
 		log.Println(info.Name())
 		if _, ok := b[info.Name()]; !ok && !info.IsDir() {
-			bs, err := ioutil.ReadFile(info.Name())
+			bs, err := ioutil.ReadFile(path.Join(p, info.Name()))
 			if err != nil {
 				return err
 			}
